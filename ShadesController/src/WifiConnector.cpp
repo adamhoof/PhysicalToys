9091d55a2f5e754be5acb5f9428d79d760e65d45
adamhoof <adam.hofman1@gmail.com>
2022-01-08 09:53:20 +0100

    remove static ip, add hostname

--- ShadesController/src/WifiConnector.cpp
+++ ShadesController/src/WifiConnector.cpp
@@ -4,11 +4,12 @@ WifiConnector::WifiConnector() = default;
 
 void WifiConnector::connect()
 {
-    if (!WiFi.config(staticIP, gateway, subnet, dns, dns)) {
-        Serial.println("Wifi config failed :(");
-    }
-
     WiFiClass::mode(WIFI_STA);
+
+    String wifiHostname = "BedroomShades";
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+    WiFi.setHostname(wifiHostname.c_str());
+
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
     Serial.println("Connecting to Wi-Fi");
